c1cd9a8be0262025d051d5b8c40c351b3a809fd4
nGyu <vpdls1511@gmail.com>
2022-05-18 18:18:37 +0900

    feat(vpdls1511) : 환경변수 설정

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: path.join(__dirname, '/.env') })
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+const SERVICE_KEY = process.env.SERVIC_EKEY;
+
 app.get("/", (req, res, next) => {
+  console.log(SERVICE_KEY);
   res.send("Test");
 });
 
