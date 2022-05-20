39bf0c783c122bf63188603d455a794d66193e58
nGyu <vpdls1511@gmail.com>
2022-05-20 16:01:19 +0900

    feat(vpdls1511) : 전주 동물병원 데이터 DB에 저장

--- apis/JeonJuHospital.js
+++ apis/JeonJuHospital.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const convert = require('xml-js');
 const request = require('request');
 
-const pool = require('./config/connection')
+const getConnection = require('./config/connection')
 
 dotenv.config({path: path.join(__dirname, '../.env')})
 
@@ -14,7 +14,7 @@ const jjHospital = () => {
 
   let queryParams = '?' + encodeURIComponent('serviceKey') + '=' + SERVICE_KEY; /* Service Key*/
   queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
-  queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('4'); /* */
+  queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /* */
 
   request({
     url: URL + queryParams,
@@ -24,10 +24,30 @@ const jjHospital = () => {
     const xmlToJson = convert.xml2json(xml, {compact: true, spaces: 2});
     const jsonData = JSON.parse(xmlToJson).rfcOpenApi.body.data.list;
     jsonData.map(i => {
-      console.log(i.apiDongName._text)
+      console.log(i)
+      const data = [
+        i.apiNewAddress._text,
+        parseFloat(i.apiLng._text),
+        parseFloat(i.apiTel._text),
+        i.apiDongName._text,
+        i.apiRegDate._text,
+        "병원"
+      ]
+      getConnection((conn) => {
+        const sql = 'INSERT INTO hospital(hospital_address, hospital_lat, hospital_lot, hospital_name, hospital_time, type) VALUES (?,?,?,?,?,?)';
+        conn.query(sql,data, (err, doc) => {
+          if(err){ console.log(err); }
+        })
+        conn.release();
+      });
     })
   });
 
 }
+const test = () => {
+
+}
+
+
 
 module.exports = jjHospital;
--- apis/config/connection.js
+++ apis/config/connection.js
@@ -6,21 +6,28 @@ dotenv.config({path: path.join(__dirname, '../../.env')})
 const DB_HOST = process.env.DB_HOST
 const DB_PORT = process.env.DP_PORT
 const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DP_PASSWORD
+const DB_PASSWORD = process.env.DB_PASSWORD
 const DB_NAME = process.env.DB_NAME
 
 const conn = {  // mysql 접속 설정
   host: DB_HOST,
-  port: DB_PORT,
   user: DB_USER,
   password: DB_PASSWORD,
+  port: DB_PORT,
   database: DB_NAME
 };
 
 const pool = mysql.createPool(conn);
 
-exports.getConnectionPool = (callback) => {
-  pool.getConnection((err, conn) => {
-    if(!err) callback(conn)
+function getConnection(callback) {
+  pool.getConnection(function (err, conn) {
+    if(err){
+      console.log(err)
+    }
+    if(!err) {
+      callback(conn);
+    }
   });
 }
+
+module.exports = getConnection;
